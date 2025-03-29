395ea639fecce863e70df6ecdc7cb1af27548a78
umang <umangkumar386@gmail.com>
2025-03-30 01:33:26 +0530

    Completed

---
+++ Frontend/src/home/Leftpart/Logout.jsx
@@ -0,0 +1,35 @@
+import axios from "axios";
+import React, { useState } from "react";
+import { BiLogOutCircle } from "react-icons/bi";
+import Cookies from "js-cookie"
+import toast from "react-hot-toast";
+
+function Logout() {
+  const [loading,setLoading]=useState(false)
+  const handleLogout=async()=>{
+    setLoading(true)
+    try{
+      const res= await axios.post("/api/user/logout");
+      localStorage.removeItem("ChatApp")
+      Cookies.remove("jwt");
+      setLoading(false)
+      toast.success("Logged out successfully");
+      window.location.reload();
+    }catch(error){
+      console.log("Error in logout",error)
+      toast.error("Error in logging out")
+    }
+  }
+  return (
+    <div className="h-[10vh]">
+      <div>
+        <BiLogOutCircle
+          className="text-5xl text-white hover:bg-slate-700 duration-300 cursor-pointer rounded-full p-2 ml-2 mt-1"
+          onClick={handleLogout}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Logout;
